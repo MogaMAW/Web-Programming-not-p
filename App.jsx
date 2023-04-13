a6730c6ae08166bfddf120051ffc287d37328b12
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-04-14 00:54:42 +1000

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -6,7 +6,7 @@ import * as React from 'react'
 //import {CandidatesForm} from './components/candidates/candidatesForm/CandidatesForm';
 //import {SideNav} from './components/sidenav/sidenavigation/SideNav';
 //import LogIn from './components/login/loginpage/LogIn'
-import Sidebar from './components/sidenav/sidenavigation/SideBar';
+//import Sidebar from './components/sidenav/sidenavigation/SideBar';
 import BallotPage from './components/ballot/BallotPage';
 //import AssitCR from './components/ballot/AssitCR'
 //import DashBoard from './components/Voters/votersform/DashBoard'
